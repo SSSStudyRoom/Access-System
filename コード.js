18d42c0961fe26a7beb1890ad3b2d4881a1515ee
yutomita-sss <yuto.mita@sss-edu.org>
2026-09-25 18:30:21 +0900

    webhookのurl削除

--- コード.js
+++ コード.js
@@ -50,7 +50,7 @@ function getPersonalSpreadsheet() {
 //   入退室通知用。WebhookのURLは秘密情報なのでGAS内のみで保管する
 //   （リポジトリには絶対にアップロードしないこと）
 // ====================================================================
-const GCHAT_WEBHOOK_URL = 'https://chat.googleapis.com/v1/spaces/AAQAUHWNd34/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=Cj4ffnZEhNIzEAtmcwWLnKy-TEu_r08HKZsDPVB9FoM';
+const GCHAT_WEBHOOK_URL = '';
 
 // ====================================================================
 // 🔗 生徒マイページのベースURL
